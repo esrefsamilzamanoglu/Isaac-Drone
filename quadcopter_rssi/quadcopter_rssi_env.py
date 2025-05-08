e3571203eb1281496ba22c3ff234438af70e3c9c
esrefsamilzamanoglu <esrefsamilzamanoglu@gmail.com>
2025-05-08 18:36:43 +0300

    simple_room scene import test

--- quadcopter_rssi/quadcopter_rssi_env.py
+++ quadcopter_rssi/quadcopter_rssi_env.py
@@ -79,22 +79,15 @@ class QuadcopterRSSIEnvCfg(DirectRLEnvCfg):
         ),
     )
     terrain = TerrainImporterCfg(
-        prim_path="/World/ground",
+        prim_path="/World/envs/env_0",
         terrain_type="usd",
         usd_path=USD_PATH,
         collision_group=-1,
-        physics_material=sim_utils.RigidBodyMaterialCfg(
-            friction_combine_mode="multiply",
-            restitution_combine_mode="multiply",
-            static_friction=1.0,
-            dynamic_friction=1.0,
-            restitution=0.0,
-        ),
         debug_vis=False,
     )
 
     # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=64, env_spacing=6, replicate_physics=True)
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=64, env_spacing=8, replicate_physics=True)
 
     # robot
     robot: ArticulationCfg = CRAZYFLIE_CFG.replace(prim_path="/World/envs/env_.*/Robot")
@@ -146,7 +139,7 @@ class QuadcopterRSSIEnv(DirectRLEnv):
         self.cfg.terrain.env_spacing = self.scene.cfg.env_spacing
         self._terrain = self.cfg.terrain.class_type(self.cfg.terrain)
         # clone and replicate
-        self.scene.clone_environments(copy_from_source=False)
+        self.scene.clone_environments(copy_from_source=True)
         # add lights
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
         light_cfg.func("/World/Light", light_cfg)
