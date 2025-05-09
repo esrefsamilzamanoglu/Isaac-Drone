ad191cd08104e1f71c335922944b612fcdf0e0b4
esrefsamilzamanoglu <esrefsamilzamanoglu@gmail.com>
2025-05-09 15:40:43 +0300

    repo rename modules

--- quadcopter_rssi/__init__.py
+++ quadcopter_rssi/__init__.py
@@ -1,26 +1,25 @@
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""
-Quacopter environment.
-"""
-
 import gymnasium as gym
 
 from . import agents
 
-##
-# Register Gym environments.
-##
-
 gym.register(
-    id="Isaac-Quadcopter-RSSI-Direct-v0",
-    entry_point=f"{__name__}.quadcopter_rssi_env:QuadcopterRSSIEnv",       
+    id="Isaac-Drone-Simple-RSSI-Seeker",
+    entry_point=f"{__name__}.simple_rssi_seeker:QuadcopterRSSIEnv",       
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.quadcopter_rssi_env:QuadcopterRSSIEnvCfg",  
+        "env_cfg_entry_point": f"{__name__}.simple_rssi_seeker:QuadcopterRSSIEnvCfg",  
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_lstm_cfg:QuadcopterPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Drone-Sionna-RSSI-Seeker",
+    entry_point=f"{__name__}.sionna_rssi_seeker:QuadcopterRSSIEnv",       
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.sionna_rssi_seeker:QuadcopterRSSIEnvCfg",  
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_lstm_cfg:QuadcopterPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
